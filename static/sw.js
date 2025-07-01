e317a50e4319233f0840a8be98dbd6b713263590
Slava <slavathedeveloper@gmail.com>
2025-07-01 13:41:38 +0300

    add service worker

---
+++ static/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'lib-cache-5bcf1d7c-d6ed-42b0-9ac8-f968d5e5c389';
+const PRECACHE_URLS = [
+  '/',
+  '/404.html',
+  '/manifest.json',
+  '/js/theme-switch.js',
+  '/favicon/android-chrome-192x192.png',
+  '/assets/combined.min.780ace2e4c5eb5073b4c8b0fb1a489e3797b614f878eb88e4fca98b6c0fac781.css'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(PRECACHE_URLS.map(url => `/library${url}`)))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.map(cacheName => {
+        if (cacheName !== CACHE_NAME) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.headers.get('accept').includes('text/html')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
+
